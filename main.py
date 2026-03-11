c7d5a9c99fdfbe7fb2d523f365903c85ec138d91
Kevin <tu_correo@dominio.com>
2026-03-10 20:46:49 -0500

    Validación de credenciales

---
+++ main.py
@@ -0,0 +1,10 @@
+user = "kevinalexandergaviriarodriguez@gmail.com"
+password = "kevin20056*"
+
+user_input = input()
+password_input = input()
+
+if user_input == user and password_input == password:
+    print("Sesión exitosa")
+else:
+    print("Contraseña o usuario incorrecta")
